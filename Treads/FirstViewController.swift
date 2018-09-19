1a748d206d4966631a173137c5d653f252119920
Steven Phan <ptviet@live.com>
2018-09-19 13:02:43 +1000

    Initial Commit

---
+++ Treads/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Treads
+//
+//  Created by Steven Phan on 19/9/18.
+//  Copyright © 2018 SP. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
---
+++ Treads/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Treads
+//
+//  Created by Steven Phan on 19/9/18.
+//  Copyright © 2018 SP. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
